02650dbe7977bfad6b267d6b918dd5e931388a06
Priscila Alfaro <priscila24n@hotmail.com>
2022-02-10 16:10:36 +0100

    fix: update BASE_URL

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,4 +1,4 @@
 // const BASE_URL = "http://localhost:8000"
 
-const BASE_URL ="https://bookstore-project-backend.herokuapp.com/"
+const BASE_URL ="https://bookstore-project-backend.herokuapp.com"
 export const API_URL = (slug) => `${BASE_URL}/${slug}`;
